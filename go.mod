d484c064ee79931741f83366c99f0205a4eb9cae
VTRyo <vtryo.work@gmail.com>
2024-01-14 19:39:10 +0900

    use slack-go

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/VTRyo/AutoJoinzy
 
 go 1.19
 
-require github.com/jarcoal/httpmock v1.3.1 // indirect
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jarcoal/httpmock v1.3.1 // indirect
+	github.com/slack-go/slack v0.12.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,56 +1,42 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
+	"github.com/slack-go/slack"
+	"log"
 	"os"
 )
 
-type SlackAPIResponse struct {
-	OK    bool   `json:"ok"`
-	Error string `json:"error,omitempty"`
+func main() {
+	apiToken := os.Getenv("SLACK_TOKEN")
+	api := slack.New(apiToken)
+
+	channelIsToJoin := []string{"times_ryo", "programming"}
+
+	for _, channelName := range channelIsToJoin {
+		channelID, err := getChannelID(api, channelName)
+		if err != nil {
+			log.Fatalf("Failed to find channel: %s, error:%v", channelName, err)
+		}
+
+		_, _, _, err = api.JoinConversation(channelID)
+		if err != nil {
+			log.Fatalf("Failed to join channel: %s error: %v", channelName, err)
+		}
+		fmt.Printf("Joined channel: %s\n", channelName)
+	}
 }
 
-func main() {
-	token := os.Getenv("SLACK_TOKEN") // Only channel:write
-	channels := []string{""}          // channel ID
+func getChannelID(api *slack.Client, channelName string) (string, error) {
+	channels, _, err := api.GetConversations(&slack.GetConversationsParameters{})
+	if err != nil {
+		return "", err
+	}
 
 	for _, channel := range channels {
-		if err := joinChannel(token, channel); err != nil {
-			fmt.Printf("Failed to join channel %s: %s\n", channel, err)
-		} else {
-			fmt.Printf("Joined channel %s successfully\n", channel)
+		if channel.Name == channelName {
+			return channel.ID, nil
 		}
 	}
-}
-
-func joinChannel(token, channelName string) error {
-	url := "https://slack.com/api/conversations.join"
-	requestBody, _ := json.Marshal(map[string]string{
-		"channel": channelName,
-	})
-
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
-	req.Header.Add("Authorization", "Bearer "+token)
-	req.Header.Add("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-	var apiResponse SlackAPIResponse
-	json.Unmarshal(body, &apiResponse)
-
-	if !apiResponse.OK {
-		return fmt.Errorf("API Error: %s", apiResponse.Error)
-	}
-
-	return nil
+	return "", fmt.Errorf("channel not found")
 }
