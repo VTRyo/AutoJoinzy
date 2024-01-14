c57afabf8624edde465764965ddc432badea9ab3
VTRyo <vtryo.work@gmail.com>
2024-01-14 20:07:20 +0900

    load from config.yaml

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/slack-go/slack"
+	"gopkg.in/yaml.v3"
 	"log"
 	"os"
 )
 
+type Config struct {
+	Channels []string `yaml:"channels"`
+}
+
 func main() {
-	apiToken := os.Getenv("SLACK_TOKEN")
+	apiToken := os.Getenv("SLACK_TOKEN") // channels:write channels:read
 	api := slack.New(apiToken)
 
-	channelIsToJoin := []string{"times_ryo", "programming"}
+	channelIsToJoin := getChannelNames()
 
 	for _, channelName := range channelIsToJoin {
 		channelID, err := getChannelID(api, channelName)
@@ -19,11 +24,14 @@ func main() {
 			log.Fatalf("Failed to find channel: %s, error:%v", channelName, err)
 		}
 
-		_, _, _, err = api.JoinConversation(channelID)
+		_, _, warn, err := api.JoinConversation(channelID)
 		if err != nil {
-			log.Fatalf("Failed to join channel: %s error: %v", channelName, err)
+			log.Printf("Failed to join channel: %s, error: %v", channelName, err)
+		} else if warn[0] == "already_in_channel" {
+			fmt.Printf("Already joined channel: %s\n", channelName)
+		} else {
+			fmt.Printf("Joined channel: %s\n", channelName)
 		}
-		fmt.Printf("Joined channel: %s\n", channelName)
 	}
 }
 
@@ -39,4 +47,18 @@ func getChannelID(api *slack.Client, channelName string) (string, error) {
 		}
 	}
 	return "", fmt.Errorf("channel not found")
+}
+
+// Load config.yaml
+func getChannelNames() []string {
+	data, err := os.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return config.Channels
 }
